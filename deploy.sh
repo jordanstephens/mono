a1b155b0472a719f69149214f9abfb90f95002f6
Jordan Stephens <jordan@stephens.io>
2015-11-12 10:49:27 -0500

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ lein clean && lein cljsbuild once min
 cp -r resources/public /tmp/
 git checkout gh-pages
 cp -r /tmp/public/* .
-git commit -am "automatic update"
+git commit -am "deploy - $(date)"

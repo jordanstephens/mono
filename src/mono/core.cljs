5821f44437380ad32f3a8bc0bb2d111a01b6ee1d
Jordan Stephens <jordan@stephens.io>
2015-11-12 21:47:44 -0500

    adding waveform toggle

--- src/mono/core.cljs
+++ src/mono/core.cljs
@@ -10,31 +10,39 @@
 (defonce octave-offset-maximum 2)
 
 ; use an array-map to maintain key order
-(defonce keyboard-map (array-map
-  90  {:label "z" :x 0}
-  83  {:label "s" :x 0.5}
-  88  {:label "x" :x 1}
-  68  {:label "d" :x 1.5}
-  67  {:label "c" :x 2}
-  86  {:label "v" :x 3}
-  71  {:label "g" :x 3.5}
-  66  {:label "b" :x 4}
-  72  {:label "h" :x 4.5}
-  78  {:label "n" :x 5}
-  74  {:label "j" :x 5.5}
-  77  {:label "m" :x 6}
-  188 {:label "," :x 7}
-))
+(defonce keyboard-map
+  (array-map
+    90  {:label "z" :x 0}
+    83  {:label "s" :x 0.5}
+    88  {:label "x" :x 1}
+    68  {:label "d" :x 1.5}
+    67  {:label "c" :x 2}
+    86  {:label "v" :x 3}
+    71  {:label "g" :x 3.5}
+    66  {:label "b" :x 4}
+    72  {:label "h" :x 4.5}
+    78  {:label "n" :x 5}
+    74  {:label "j" :x 5.5}
+    77  {:label "m" :x 6}
+    188 {:label "," :x 7}))
 
-(defonce control-key-map (array-map
-  219 {:label "["}
-  221 {:label "]"}
-))
+(defonce octave-key-map
+  (array-map
+    219 {:label "["}
+    221 {:label "]"}))
+
+(defonce waveform-key-map
+  (array-map
+    81 {:label "q"}))
+
+(defonce control-key-map
+  (merge octave-key-map waveform-key-map))
 
 ; app state
 (defonce keyboard-keys-down (atom #{}))
 (defonce control-keys-down (atom #{}))
 (defonce octave-offset (atom 0))
+(defonce waveform-index (atom 0))
 
 
 
@@ -65,6 +73,12 @@
 (defn with-control-code [event f]
   (with-key-code-in-map event control-key-map f))
 
+(defn change-waveform []
+  (reset! waveform-index (mod (+ @waveform-index 1) 3)))
+
+(defn current-waveform []
+  (get synth/waveforms @waveform-index))
+
 (defn octave-up []
   (if (< @octave-offset octave-offset-maximum)
     (reset! octave-offset (+ @octave-offset 1))))
@@ -75,6 +89,7 @@
 
 (defn map-control-key [key-code]
   (case key-code
+    81 (change-waveform)
     219 (octave-down)
     221 (octave-up)))
 
@@ -83,7 +98,7 @@
     (with-keyboard-code event (fn [key-code]
       (do
         (swap! keyboard-keys-down conj key-code)
-        (synth/note-on (key-code-frequency key-code)))))
+        (synth/note-on (key-code-frequency key-code) (:name (current-waveform))))))
     (with-control-code event (fn [key-code]
       (do
         (map-control-key key-code)
@@ -96,28 +111,39 @@
         (swap! keyboard-keys-down disj key-code)
         (if (empty? @keyboard-keys-down)
             (synth/note-off)
-            (synth/note-on (key-code-frequency (first @keyboard-keys-down)))))))
+            (synth/note-on (key-code-frequency (first @keyboard-keys-down)) (:name (current-waveform)))))))
     (with-control-code event (fn [key-code]
       (do
         (swap! control-keys-down disj key-code))))))
 
+(defn waveform-control-keys []
+  [:div {:className "control-group waveform"}
+    [:div {:className "control-label"}
+      [:ul {:className "waveform-value"}
+        [:li (:label (current-waveform))]]]
+    [:ol {:className "control-keys"}
+      [:li {:key 0 :data-key-down (contains? @control-keys-down 81)}
+        [:span "q"]]]])
 
+(defn octave-control-keys []
+  [:div {:className "control-group octave"}
+    [:div {:className "control-label"}
+      [:ul {:className "octave-offset-value"}
+        [:li {:data-light (if (contains? #{-2 -1} @octave-offset) "true" "false")}"•"]
+        [:li {:data-light (if (contains? #{-1 0 1} @octave-offset) "true" "false")}"•"]
+        [:li {:data-light (if (contains? #{1 2} @octave-offset) "true" "false")}"•"]
+      ]]
+    [:ol {:className "control-keys"}
+      (doall (map-indexed
+        (fn [idx [key-code {label :label}]]
+          [:li {:key idx :data-key-down (contains? @control-keys-down key-code)}
+            [:span label]])
+        octave-key-map))]])
 
 (defn control-keys []
   [:div {:className "control-keys-container"}
-    [:div {:className "control-group octave"}
-      [:div {:className "control-label"}
-        [:ul {:className "octave-offset-value"}
-          [:li {:data-light (if (contains? #{-2 -1} @octave-offset) "true" "false")}"•"]
-          [:li {:data-light (if (contains? #{-1 0 1} @octave-offset) "true" "false")}"•"]
-          [:li {:data-light (if (contains? #{1 2} @octave-offset) "true" "false")}"•"]
-        ]]
-      [:ol {:className "control-keys"}
-        (doall (map-indexed
-          (fn [idx [key-code {label :label}]]
-            [:li {:key idx :data-key-down (contains? @control-keys-down key-code)}
-              [:span label]])
-          control-key-map))]]])
+    [waveform-control-keys]
+    [octave-control-keys]])
 
 (defn keyboard []
   [:ol {:className "keyboard"}
--- src/mono/synth.cljs
+++ src/mono/synth.cljs
@@ -2,11 +2,16 @@
 
 (defonce context (js/AudioContext.))
 
+(defonce waveforms
+  [{:name "sine" :label "∿"}
+   {:name "triangle" :label "⋀"}
+   {:name "square" :label "⊓"}])
+
 (defn defosc [context]
   (let [osc (.createOscillator context)]
-    (do (.start osc 0)
-        (set! (.-type osc) "triangle")
-        osc)))
+    (do
+      (.start osc 0)
+      osc)))
 
 (defn defgain [context]
   (let [gain (.createGain context)]
@@ -19,11 +24,12 @@
 (.connect osc1 osc1-gain)
 (.connect osc1-gain (.-destination context))
 
-(defn note-on [frequency]
+(defn note-on [frequency waveform]
   (let [now (.-currentTime context)
         osc-frequency (.-frequency osc1)
         osc-gain (.-gain osc1-gain)]
     (do
+      (set! (.-type osc1) waveform)
       (.setValueAtTime osc-frequency frequency now)
       (.setValueAtTime osc-gain 1.0 now))))
 
